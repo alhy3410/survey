7c4f5b6c9019052024c3d132010713bd6fbb38e4
andrew lee & ian bevel <student@epicodus.com>
2015-05-12 11:03:56 -0700

    add two columns to the questions table, allow for creation of surveys

--- app.rb
+++ app.rb
@@ -7,5 +7,19 @@ also_reload("lib/**/*.rb")
 require("pg")
 
 get('/') do
+  @surveys = Survey.all()
   erb(:index)
+end
+
+post('/') do
+  survey_name = params.fetch('survey_name')
+  survey = Survey.new(:survey_name => survey_name)
+  survey.save()
+  @surveys = Survey.all()
+  erb(:index)
+end
+
+get('/surveys/:id') do
+  @survey = Survey.find(params.fetch('id').to_i())
+  erb(:survey_questions_list)
 end
